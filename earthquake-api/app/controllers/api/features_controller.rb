5286f70880ba20c531557feb84cdc304c42c2407
Willy Maykros Romero <94816128+maikcodes@users.noreply.github.com>
2024-04-08 21:23:14 -0500

    Change feature controller to api dir

--- earthquake-api/app/controllers/api/features_controller.rb
+++ earthquake-api/app/controllers/api/features_controller.rb
@@ -1,4 +1,4 @@
-class FeaturesController < ApplicationController
+class Api::FeaturesController < ApplicationController
   before_action :set_feature, only: %i[ show update destroy ]
 
   # GET /features
